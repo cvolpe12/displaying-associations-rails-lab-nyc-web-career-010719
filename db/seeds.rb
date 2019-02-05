942a8cc665564085c3661e1e4f15604e502ddc1d
cvolpe12 <christopher.volpe12@gmail.com>
2019-02-05 16:52:31 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Addings data"
+
+Artist.create(name: "Kendric Lamar")
+Artist.create(name: "Foo Fighters")
+Artist.create(name: "Michael Jackson")
+Artist.create(name: "Fleetwood Mac")
+
+
+
+
+Song.create(title: "Poetic Justice", artist_id: 1)
+Song.create(title: "My Hero", artist_id: 2)
+Song.create(title: "Man in the Mirror", artist_id: 3)
+Song.create(title: "HUMBLE", artist_id: 1)
+Song.create(title: "Dreams", artist_id: 4)
+Song.create(title: "The Chain", artist_id: 4)
+
+
+puts "Data added"
